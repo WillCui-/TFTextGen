11d8724038c3252ca3e5ac0129d57fef518399c2
U-COMPUTER\Will <wyfcui@gmail.com>
2019-03-20 23:12:59 -0400

    First

---
+++ predict.py
@@ -0,0 +1,79 @@
+import numpy as np
+np.random.seed(42)
+import tensorflow as tf
+tf.set_random_seed(42)
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation
+from keras.layers import LSTM, Dropout
+from keras.layers import TimeDistributed
+from keras.layers.core import Dense, Activation, Dropout, RepeatVector
+from keras.optimizers import RMSprop
+# import matplotlib.pyplot as plt
+import pickle
+import sys
+import heapq
+import seaborn as sns
+from pylab import rcParams
+import random
+
+random.seed(a=99)
+
+path = 'nietzsche.txt'
+text = open(path).read().lower()
+print('corpus length:', len(text))
+
+chars = sorted(list(set(text)))
+char_indices = dict((c, i) for i, c in enumerate(chars))
+indices_char = dict((i, c) for i, c in enumerate(chars))
+
+SEQUENCE_LENGTH = 40
+
+model = load_model('keras_model.h5')
+
+history = pickle.load(open("history.p", "rb"))
+
+def prepare_input(text):
+    x = np.zeros((1, SEQUENCE_LENGTH, len(chars)))
+
+    for t, char in enumerate(text):
+        x[0, t, char_indices[char]] = 1.
+
+    return x
+
+
+def sample(preds, top_n=3):
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds)
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+
+    return heapq.nlargest(top_n, range(len(preds)), preds.take)
+
+
+def predict_completion(text):
+    original_text = text
+    generated = text
+    completion = ''
+    while True:
+        x = prepare_input(text)
+        preds = model.predict(x, verbose=0)[0]
+        next_index = sample(preds, top_n=1)[0]
+        next_char = indices_char[next_index]
+
+        text = text[1:] + next_char
+        completion += next_char
+
+        if len(original_text + completion) + 2 > len(original_text) and next_char == ' ':
+            return completion
+
+def predict_completions(text, n=3):
+    x = prepare_input(text)
+    preds = model.predict(x, verbose=0)[0]
+    next_indices = sample(preds, n)
+    return [indices_char[idx] + predict_completion(text[1:] + indices_char[idx]) for idx in next_indices]
+
+output = "                                        "
+for i in range(100):
+    x = predict_completions(output[-SEQUENCE_LENGTH:].lower(), 5)
+    output += x[random.randint(0, 4)]
+print(output)
